38abf752e1567c0456f11c6435005990535d3800
leconghieu99 <96342175+leconghieu99@users.noreply.github.com>
2024-10-28 00:13:08 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -18,6 +18,7 @@ const fetchData = () => {
                 // Check if the iteration is over
                 // Run amimation if so
                 if (dataArr.length === dataArr.indexOf(customData) + 1) {
+                    document.querySelector('.song').play();
                     animationTimeline();
                 }
             });
